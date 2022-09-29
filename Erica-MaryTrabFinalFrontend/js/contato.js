21a4550d928b2ee5bd96c8fd6568e80fb59d4cfe
vTekashi <victortiagoferreira@gmail.com>
2022-09-29 19:35:50 -0300

    corrigindo

---
+++ Erica-MaryTrabFinalFrontend/js/contato.js
@@ -0,0 +1,8 @@
+const name = document.getElementById('nome');
+const email = document.getElementById('email');
+const telefone = document.getElementById('telefone');
+
+function CheckInput(){
+    const nameValue = name.value;
+
+}
